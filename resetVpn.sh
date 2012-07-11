a4d71d8ea26dd3e5c8e81c92c2fa291c32678236
Guimin Lin <lincank@gmail.com>
2012-07-11 13:39:51 +0800

    update anti-block list and script

--- resetVpn.sh
+++ resetVpn.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
 # Author: Guimin Lin
 # Date: 07/01/2012
 # Reset openvpn connection in dd-wrt router, for autoddvpn
+# 
+# cron:
+# 00 5 * * 0-6/2 root /jffs/openvpn/resetVpn.sh 2>&1 >> /tmp/vpn_reset.log
 
-#!/bin/sh
-
+echo "*********** VPN reset script starts ***********"
 echo "[`date`]: Killing openvpn ..."
 OPENVPN_NUM=`ps | grep openvpn | wc -l`
 if [ ${OPENVPN_NUM} -ne 1 ]; then
@@ -13,3 +16,4 @@ sleep 1
 
 openvpn --config /jffs/openvpn/openvpn.conf --daemon 
 echo "[`date`]: Openvpn started! Reconnect succeed! End script"
+echo "*********** VPN reset script ends ***********"
