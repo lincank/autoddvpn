3102ce68204fc55abbe1cb16f373c50c1bfa3e85
lincank <lincank@gmail.com>
2012-11-02 16:37:54 +0800

    improve add2route script

--- scripts/add2route.sh
+++ scripts/add2route.sh
@@ -8,20 +8,40 @@
 usage()
 {
     echo "Usage: add2route.sh -[h|n] ip"
-    echo e.g. 
-    echo host: add2route.sh h 2.2.2.2 
-    echo net: add2route.sh n 2.2.2.0/24
 }
+
+if [ $# -lt 2 ]; then
+    usage
+    exit 1
+fi
+
+MODE=''
+while getopts ":nh" opt; do
+     case $opt in
+         h) 
+             MODE="h"
+             ;;
+         n)
+             MODE="n"
+             ;;
+         \?) 
+             echo "Invalid option: -$OPTARG" >&2
+             usage
+             exit 1
+             ;;
+     esac
+done
+shift `expr $OPTIND - 1`
 
 # VPN gateway address
 VPNGW=$(ifconfig tun0 |grep -Eo "P-t-P:([0-9.]+)" | cut -d: -f2)
 
-if [ ${1} == "h" ]; then
-    echo "route add -host ${2} gw $VPNGW"
-    route add -host ${2} gw $VPNGW
-elif [ ${1} == "n" ]; then
-    echo "route add -net ${2} gw $VPNGW"
-    route add -net ${2} gw $VPNGW 
+if [ ${MODE} == "h" ]; then
+    echo "route add -host ${1} gw $VPNGW"
+    route add -host ${1} gw $VPNGW
+elif [ ${MODE} == "n" ]; then
+    echo "route add -net ${1} gw $VPNGW"
+    route add -net ${1} gw $VPNGW 
 else
     usage 
 fi
