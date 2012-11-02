35545dde422555f5d00a12e8490dc0acc850da13
lincank <lincank@gmail.com>
2012-11-02 16:46:08 +0800

    update doc

--- scripts/cp2router.sh
+++ scripts/cp2router.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+# Author: Guimin Lin
+# Date: 2012/7/6
+# 
+# Helper script for copying files to route in /jffs/openvpn dir
 
 # router ip
 GW=`netstat -rn | grep UG |tr -s ' '|cut -d ' ' -f2`
 
-scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${1} root@${GW}:/jffs/openvpn/
-
-
-#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${GW} "/jffs/openvpn/resetVpn.sh"
+scp -r -o StrictHostKeyChecking=no  ${@} root@${GW}:/jffs/openvpn/
 
 
 
